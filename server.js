d5612440aa6d700b9c1a577e54e289ff635ebb68
zd07 <zahidibnrizwan@gmail.com>
2026-04-26 14:05:32 +0530

    Updated PORT for deployment

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get("/", (req, res) => {
   res.send("DevSphere Server is Running Successfully");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
