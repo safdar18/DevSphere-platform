63ea9bac9f1b2edddb3816ac25fcd36d2a8bb11e
zd07 <zahidibnrizwan@gmail.com>
2026-04-26 17:42:09 +0530

    Added status API

--- server.js
+++ server.js
@@ -15,3 +15,10 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
+
+app.get("/api/status", (req, res) => {
+  res.json({
+    message: "DevSphere API is working",
+    status: "success"
+  });
+});
